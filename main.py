01c88e8206136c076e2c996bfa5efaf91f223876
Mr.Harsh <81058137+mr-harsh-codes@users.noreply.github.com>
2021-06-13 06:28:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 import discord
 import os , json , random
 from discord.ext import commands
-from functions.function import Events
+from functions.events import Events
+from functions.function import func
 
 bot = commands.Bot(command_prefix='m!', case_insensitive=True)
 bot.remove_command('help')
 
-# Files needed -
-# main.py
-# greet.json
-# profile.json
-# channels.json
-
-events = Events(bot)
-bot.event(events.on_command_error)
-bot.event(events.on_guild_leave)
+event = Events(bot)
+bot.event(event.on_command_error)
+bot.event(event.on_guild_leave)
+events = func()
 
 @bot.event
 async def on_ready():
     print("We are ready to Go!")
 
 async def on_message(message):
-    if bot.user.mentioned_in(message):
-        with open(".\main_resources\greet.json","r") as f:
-            greet = json.load(f)
-        await message.channel.send(random.choice(greet["response"]))
-    await bot.process_commands(message)
-
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
-        bot.load_extension(f"cogs.{filename[:-3]}")
+    if events.is_not_restricted_channel(message):
+        if bot.user.mentioned_in(message):
+            await message.channel.send("Hello I am Minecord the ultimate Minecraft Discord Bot, my prefix is ```m!```.")
+        elif "start" in message.content[1:] or "help" in message.content[1:]:
+            await bot.process_commands(message)
+        elif events.has_profile(message):
+            await bot.process_commands(message)
+        else:
+            await message.channel.send(f"{message.author.mention} you haven't created a profile, create a profile by the ```start``` command.")
 
 if __name__ == '__main__':
-    bot.run('TOKEN')
+    bot.run('Token')
+
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+    print("Loaded All Cogs")
+
