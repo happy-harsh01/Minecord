e6d27d7a87e65f7101959573d2a7749482247cea
Mr.Harsh <81058137+mr-harsh-codes@users.noreply.github.com>
2021-10-07 20:46:42 +0530

    Update game_functions.py

--- cogs/functions/game_functions.py
+++ cogs/functions/game_functions.py
@@ -1,112 +1,173 @@
 from discord.ext import commands
 import json
-
+profile = ".//main_resources//profiles.json"
+info = ".//main_resources//info.json"
 
 class GameFunction:
+    
+    async def change_name(self, ctx, name):
+    """Changes user name"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        profiles[str(ctx.author.id)]["name"] = name
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
 
-    @staticmethod
-    def get_profiles():
-        with open(".\\main_resources\\profile.profile.json", "r") as f:
-            return json.load(f)
-    
-    async def break_tool(self, tool):
-        """Breaks a tool , removes it from inventory and announces it."""
-        pass
-    
-    async def tool_durability_modifier(self, value, type):
-        """Increase or decreases Tools durability equipped by user. Tool health increases only when it is enchanted by 
-        unbreaking and decreases when it gets damage."""
-        pass
-    
-    async def armour_durability_modifier(self, value, type):
-        """Increase or decreases Armour health(durability) equipped by user. Armour health increases only when it 
-        is enchanted by unbreaking and decreases when it gets damage."""
-        pass
-    
-    async def armour_breaker(self, armour):
-        """Breaks user's armour and decreases his health increased by wearing it and also announces it."""
-        pass
-
-    async def armour_equiptor(self, amour):
-        """When user equipts armour his health is increased."""
-        pass
-    
-    async def increase_health(self, health_point):
-        """Increases the players health when he eats something."""
-        pass
-    
-    async def kill(self, ):
-        """Kills the user, his inventory items are stored for sometime"""
-        pass
-    
-    async def create_profile(self, ctx, name):
-        """Creates user's profile contianing his data."""
-        profile = {
-            "name": name,
-            "health": 100,
-            "world": "Over World",
-            "location": "Home",
-            "inv": [],
-            "places": [],
-            "builds": [],
-            "advancements": [],
-            "xp": 0
-        }
-        profiles = self.get_profiles()
-        profiles.update({str(ctx.author.id): profile})
-        with open(".\\main_resources.\\profile.json", "w") as f:
-            json.dump(profile, f)
-        print(f"New profile created for {ctx.author}")
-    
-    async def xp(self, ctx, xp):
-        """Increases or decreases the user's level"""
-        profiles = self.get_profiles()
-        profiles[ctx.author.id]["xp"] += xp
-    
-    async def fight(self, ):
-        """Fights two player or mobs and subsequently decreses their health."""
-        pass
-    
-    async def spawn(self, ):
-        """Spawns mob randomly when they chat and adds mob to the list."""
-        pass
-
-    async def inv_manager(self, item, decrement=0, increment=0):
-        """Manages inventory items by adding new items or sorting them
-        If item is present modifies its quantity else adds that item in the inventory.
-        Either increment or decrement argument is requied , both are not allowed to be given."""
-        if decrement > 0 and increment > 0:
-            raise ValueError("Both increment and decrement are not allowed to be given.")
-        inv = self.get_profiles()["inv"]
-        inv_items = list(inv.keys())
-        if item in inv_items:
-            orignal_value = inv[item]
-            if increment > 0:
-                inv_items = orignal_value + increment
+    async def change_health(self, ctx, value):
+    """Changes user's health"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+            health = profiles[str(ctx.author.id)]["health"]
+        if health > 100:
+            pass
+        else:
+            if health == -value:
+                self.kill(ctx)
+                return
             else:
-                inv_items = orignal_value - decrement
-        elif increment > 0:
-            inv[item] = increment
+                health += value
+        profiles[str(ctx.author.id)]["health"] = health
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
 
-    async def add_advancement(self, advancement):
-        """Adds an advancement to the user's profile and also announces it."""
-        profile = self.get_profiles()
-        advancements = profile["advancements"]
-        advancements.append(advancement)
-        with open(".\\main_resources\\profile.profile.json", "w") as f:
-            json.dump(advancements, f)
-    
-    async def place_searcher(self, place):
-        """Searches for place in user places."""
-        profile = self.get_profiles()
-        places = profile["places"]
-        if place in places:
+    async def armour_durability_modifier(self, ctx, armour, value):
+    """Mofidfies armour durability"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+            armours = profiles[str(ctx.author.id)]["armour"]
+        if armours[armour] == -value:
+        # Breaking the armour if full durability has to be reduced
+            self.break_armour(ctx, armour)
+            return
+        else:
+            armours[armour] += value
+        profiles[str(ctx.author.id)]["armour"] = armours
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def equipt_armour(self, ctx, name):
+    """Equipts the armour and increases the max health"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def break_armour(self, ctx, name):
+    """Breaks the armour and decreases the max health"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def inv_manager(self, ctx, item, value):
+    """Increases or decreases items value in the inventory
+     Also adds new items to the inv"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        inv = profiles[str(ctx.author.id)]["inv"]
+        if item in inv:
+            if inv[item] == -value:
+                inv.pop(item)
+                if 'pickaxe' in item or 'sword' in item or 
+                'axe' in item or 'shovel' in item or 'hoe' in item:
+                    ctx.send(f"{ctx.author.mention} your {item} broke down")
+            else:
+                inv[item] += value
+        else:
+            inv.update({item: value})
+        profiles[str(ctx.author.id)]["inv"] = inv
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def inv_searcher(self, ctx, item):
+    """Searches for item in the inventory, if founded returns true else returns false"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        if item in profiles[str(ctx.author.id)]["inv"]:
             return True
         else:
             return False
-        
+    
+    async def inv_size_manager(self, ctx, size):
+    """Changes inv max size"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        profiles[str(ctx.author.id)]["inv_size"] == size
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def add_place(self, ctx, place, remove= False):
+    """Adds place in the places list of the user id"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        if remove is False:
+            profiles[str(ctx.author.id)]["places"].append(place)
+        else:
+            profiles[str(ctx.author.id)]["inv"].pop(places)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def place_searcher(self, ctx, name):
+    """Searches for place in user profile, if found returns true"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def adv_manager(self, ctx, name):
+    """Adds advancements in the user profile"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+   
+    async def adv_searcher(self, ctx, name):
+    """Searches for advancement in profile, if found returns true"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def builds_manager(self, ctx, name):
+    """Adds or removes builds from profile"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def build_searcher(self, ctx, name):
+    """Searches for builds in profile, if found returns true"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+    
+    async def location_changer(self, ctx, world=False, location=False):
+    """"Changes user's location"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
     async def level(self, ctx):
-        """Returns the user level."""
-        profile = self.get_profiles()
-        xp = profile[ctx.author.id]["xp"]
-        return str(xp/10)
+    """Returns the level of the user"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def kill(self, ctx):
+    """Kills the user and stores his inv item in temp inv for 1 min
+    Also generates a kill code"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
+
+    async def xp_manager(self, ctx, xp):
+    """Increases or decreases xp"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+        profiles[str(ctx.author.id)]["xp"] += xp
+        with open(profile, 'w') as f:
+            json.dump(profiles, f, indent=4)
