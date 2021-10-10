da217a78414dc1ebec8804a74738258ddc24576c
Mr.Harsh <81058137+mr-harsh-codes@users.noreply.github.com>
2021-10-10 16:16:06 +0530

    Update function.py

--- cogs/functions/function.py
+++ cogs/functions/function.py
@@ -33,46 +33,6 @@ class Func:
             return False
 
     @staticmethod
-    async def add_inv(ctx, item, quantity: int):
-        """Adds single item of definite quantity to inventory"""
-        with open(profile, 'r') as f:
-            inv = json.load(f)[str(ctx.author.id)]["inv"]
-        if item in inv:
-            a = inv[item]
-            inv[item] = a + quantity
-        else:
-            inv.update({item: quantity})
-        with open(profile, 'w') as f:
-            json.dump(f, inv, indent= 4)
-
-    @staticmethod
-    async def search_inv_item(ctx, *items):
-        """Searches for an item in the inventory, if item is present returns True else returns False"""
-        with open(profile, "r") as f:
-            inv = json.load(f)[str(ctx.author.id)]["inv"]
-        item_found = False
-        for item in items:
-            if item == list(inv.keys()):
-                    item_found = True
-            else:
-                item_found = False
-        return item_found
-
-    @staticmethod
-    def name(ctx):
-        with open(profile, 'r') as f:
-            name = json.load(f)[str(ctx.author.id)]["name"]
-            return name
-
-    @staticmethod
-    async def info(ctx, info_type):
-        """Returns person's info"""
-        with open(profile, 'r') as f:
-            data = json.load(f)
-            data = data[str(ctx.author.id)][info_type]
-            return data
-
-    @staticmethod
     async def is_not_restricted_channel(message):
         with open(restricted_channels, "r") as f:
             channels = json.load(f)
@@ -83,3 +43,25 @@ class Func:
                 return True
         except KeyError:
             pass
+
+    async def is_correct_channel(self, ctx):
+        """Check whether the user is playing in the correct channel or not"""
+        with open(profile, 'r') as f:
+            profiles = json.load(f)
+            loc = profiles[str(ctx.author.id)["location"]
+        with open(profiles, 'w') as f:
+            channels = json.load(f)
+            channel = channels[str(ctx.guild.id)]
+        if str(ctx.guuld.id) in channels:
+            if channel[loc] == str(ctx.channel.id):
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    async def send_embed(ctx, title, description,colour:discord.Colour , footer=False):
+        embed = discord.Embed(title=title, description=description, colour=colour)
+        if footer == True:
+            embed.set_footer(text=f"Request by {ctx.author}", icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
