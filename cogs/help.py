b67e2e697b5f3602a8c531a3a6a28b3a5483d91c
Mr.Harsh <81058137+mr-harsh-codes@users.noreply.github.com>
2021-11-14 12:41:33 +0530

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-form .functions.function import Func
+from .functions.function import Func
+import json
 func = Func()
 
 # Help Command
@@ -9,26 +10,26 @@ class Help(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def help(self, ctx, cmd=None):
+    async def help(self, ctx,*, cmd=None):
+        with open ("cogs//functions//main_resources//help.json", 'r') as f:
+                help = json.load(f)
         if cmd is None:
+            cmd = "Help" 
             fields_dict = {"Activity Commands": "```eat, health, go, craft, mine, adventure, inventory, profile```",
             "Over World Commands": "```cave, fish, crop, enchant, trade, take, chest, build```",
             "Game Settings Commands": "```start, delete, change_name```",
             "Setup Commands": "```quick_setup, auto_setup, add_restricted_chan, set_over_chan, set_nether_chan, set_end_chan```"}
-            func.send_embed(ctx, 'Help Menu',"",discord.Colour.red(),fields_dict,footer=True)
-        else:
-            with open ("cogs//functions//main_resources//help.json", 'r') as f:
-                help = json.load(f)
+            await func.send_embed(ctx, cmd+" Command",'',"red",fields=fields_dict, footer=True )
+            return 
+        else:    
             if not cmd in help:
                 await ctx.send(f"No help command for {cmd} found.")
                 return
             for help_cmd in list(help.keys()):
-                if cmd in help_cmd:
-                    search = {help_cmd: help[help_cmd]}
-                    break
-            func.send_embed(ctx, cmd+" Command",'',discord.Colour.red(),search, footer=True)
-             with open ("cogs//functions//main_resources//help.json", 'w') as f:
-                json.dump(help, f, indent=4)
-
+                if cmd.lower() in help_cmd:
+                    fields_dict = {help_cmd: help[help_cmd]}
+                    break                 
+        await func.send_embed(ctx, cmd+" Command",'',"red",fields=fields_dict, footer=True)
+      
 def setup(client):
     client.add_cog(Help(client))
