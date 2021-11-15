a547ae078b738f0e8c986b8710b3b03668e98f89
Mr.Harsh <81058137+mr-harsh-codes@users.noreply.github.com>
2021-11-15 07:59:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
-import os
+import discord
 from discord.ext import commands
-from functions.events import Events
-from functions.function import Func
-bot = commands.Bot(command_prefix='m!', case_insensitive=True, help_command=False)
-bot.remove_command('help')
+from cogs.functions.function import Func
+from cogs.functions.events import Events
+import os
+import time
+import json
+import asyncio
+from dislash import InteractionClient
 
-event = Events(bot)
-bot.event(event.on_command_error)
-bot.event(event.on_guild_leave)
-events = Func()
+from keep_alive import keep_alive
+intents = discord.Intents(messages = True, guilds = True, dm_messages = True, members = True, presences = True, dm_reactions = True, reactions = True, emojis = True ) 
+client = commands.Bot(command_prefix=['m! ','m!'],case_insensetive=True,  help_command=None, intents = intents )
+slash = InteractionClient(client) 
 
+events = Func(client)
+event = Events(client)
 
-@bot.event
+#------On Ready------#
+@client.event
 async def on_ready():
-    print("We are ready to Go!")
+  print("We are ready to go!")
+  await client.change_presence(activity= discord.Game("Minecraft"))    
 
 
-async def on_message(message):
-    if events.is_not_restricted_channel(message):
-        if bot.user.mentioned_in(message):
-            await message.channel.send("Hello I am Minecord the ultimate Minecraft Discord Bot, my prefix is ```m!```.")
-        elif "start" in message.content[2:] or "help" in message.content[2:]:
-            await bot.process_commands(message)
-        elif events.has_profile(message):
-            await bot.process_commands(message)
-        else:
-            await message.channel.send(f"{message.author.mention} you haven't created a profile, create a profile"
-                                       " by the ```start``` command and start playing.")
+#-------Events-------#
+client.event(event.on_message)
+client.event(event.on_guild_leave)
+client.event(event.on_command_error)
+client.event(event.on_command_completion)
+client.event(event.on_guild_channel_delete)
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
-        bot.load_extension(f"cogs.{filename[:-3]}")
-        print("Loaded Cog :" + filename)
-
+#------Running------#
 if __name__ == '__main__':
-    print("Running")
-    bot.run('Token')
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"): 
+            client.load_extension(f"cogs.{filename[:-3]}")
+            print(f" • Loaded Cog : {filename}", end = "\r")
+            time.sleep(.5)  
+    files = ["info", "help", "profiles", "channels", "restricted_channels"]
+    json_file = ""
+    for file in files:        
+      with open(f"cogs/functions/main_resources/{file}.json", "r") as f:
+          try: 
+            info = json.load(f)  
+          except Exception as e:
+            print(f"{file}.json : ", e)
+            print("\nAborting Mission....")
+            exit()
+    print("\n • All files working properly \n")
+    keep_alive()
+ 
+    client.run(os.environ['TOKEN'])
+      
